f8310d95974fa381e1f561dc9628b0ed090b1274
Aamir Qureshi <qureshiaamir561@gmail.com>
2025-10-24 13:04:45 +0530

    made changes for railway

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,12 @@ const passport = require('passport');
 const plm = require('passport-local-mongoose');
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/pin');
+// mongoose.connect('mongodb://127.0.0.1:27017/pin');
 
 const userSchema = new mongoose.Schema({
     username: String,
     email: String,
-    passport: String,
+    password: String,
     profileImage: String,
     posts: [
         {
